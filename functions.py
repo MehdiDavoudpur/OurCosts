bda8e49b1db56732d747db1cac84df3526ed19a3
Mehdi <davoudpour2000@gmail.com>
2023-08-21 18:33:33 +0330

    replace excel by database

--- functions.py
+++
@@ -1,8 +0,0 @@
-import openpyxl
-myWorkbook = openpyxl.Workbook()
-worksheet = myWorkbook.active
-
-
-def add_row(path, list_as_new_row):
-    worksheet.append(list_as_new_row)
-    myWorkbook.save(path)
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from functions import add_row
 from datetime import date
+import sqlite3
 
 myPath = 'ourCosts.xlsx'
 header = ['Date', 'Commodity/Service', 'Group', 'Price']
 options = ['Food', 'Clothes', 'Housing', 'Health', 'Education', 'Entertainment & Welfare', 'Transportation']
-
 
 title_bar = st.title('Our Costs'.title())
 title_bar2 = st.header('Registration and management of expenses'.title())
@@ -14,19 +13,28 @@ com_ser_text = st.text_input(label="Commodity/Service:", key='com/ser')
 selected_group = st.selectbox("Group:", options, key='group')
 price = st.text_input(label='Price:', key='price')
 add = st.button(label='Add', key='Add')
-create_dl_lnk = st.button(label='Create Download link', key='c_d_l')
+# create_dl_lnk = st.button(label='Create Download link', key='c_d_l')
+show = st.button(label='Show', key='show')
 
 mylist = [selected_date, com_ser_text, selected_group, price]
+row = [(com_ser_text, selected_group, price, selected_date)]
+
+connection = sqlite3.connect('costs.db')
+cursor = connection.cursor()
+
+
+def add_row_to_database(a_row):
+    cursor.executemany("INSERT INTO our_costs VALUES(?, ?, ?, ?)", a_row)
+    connection.commit()
+
 
 if add:
-    add_row(myPath, mylist)
+    add_row_to_database(row)
 
-if create_dl_lnk:
-    def download_excel():
-        with open('ourCosts.xlsx', 'rb') as f:
-            data = f.read()
-        st.download_button(label='Download Excel file', data=data, file_name='ourCosts.xlsx',
-                           mime='application/vnd.malformations-office document.spreadsheet.sheet')
+if show:
+    cursor.execute('SELECT * FROM our_costs')
+    data = cursor.fetchall()
+    connection.close()
 
-
-    download_excel()
+    st.write("Data from the database:")
+    st.table(data)
